20e9435f221d6ca956265bc7feda1eebc1382ed7
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-10 16:34:02 -0300

    adiciona Head nas páginas /profile e /posts/first-post

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,17 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Profile() {
-    return <Image 
-        src="/images/profile.jpg" 
-        width={400} 
-        height={400}
-        alt="Your name"/>;
+    return (
+        <>
+            <Head>
+                <title>Profile</title>
+            </Head>
+            <Image 
+                src="/images/profile.jpg" 
+                width={400} 
+                height={400}
+                alt="Your name"/>
+        </>
+    );
 }
