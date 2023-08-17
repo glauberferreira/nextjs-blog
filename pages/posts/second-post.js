8e1a671fc6f58fdb92e88030dba73e486942e6ff
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-17 16:20:15 -0300

    adiciona página posts/second-post e muda background para amarelo

---
+++ pages/posts/second-post.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../../components/layout'
+
+export default function SecondPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>Second Post</title>
+            </Head>
+            <h1>Second Post</h1>
+        </Layout>
+    );
+}
