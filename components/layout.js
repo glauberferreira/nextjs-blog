443518bcf0df0117fe110da17f31351a4384dd51
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-17 15:48:36 -0300

    adicionando CSS no layout

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,7 @@
+import styles from './layout.module.css';
+
 export default function Layout({children}){
-    return <div>{children}</div>;
+    return <div className={styles.container}>
+                {children}
+            </div>;
 }
