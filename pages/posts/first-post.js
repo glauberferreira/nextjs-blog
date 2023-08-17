a649d31e19f2e60555d5af399c16c082dabdfa9b
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-17 14:30:45 -0300

    adiciona componente Layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -17,6 +18,6 @@ export default function FirstPost() {
             />
             <h1>First Post</h1>
             <h2><Link href="/">Back to home</Link></h2>
-        </>
+        </Layout>
     );
 }
